1150227d09b915abcf5e4b780700d97b8c24eb45
raedah <raedah@raedah.local>
2017-04-12 11:27:28 -0700

    latest

--- proposals.go
+++ proposals.go
@@ -109,13 +109,48 @@ func (s *simulator) calcNextStakeDiffProposal1E() int64 {
 	        freshStakeRatio := freshStakeLastWindow / maxFreshStakePerWindow
 	*/
 
+	newDiff := float64(prevDiff) * lastRatio
+        factor := 2.0 // increase the action
+
+        if lastRatio < 1.0 {
+            newDiff = float64(prevDiff) * (1.0 - (((1.0 - lastRatio) * factor) * (1/targetRatio)))
+        }
+        if lastRatio > 1.0 {
+            newDiff = float64(prevDiff) * (1.0 + (((lastRatio - 1.0) * factor) * targetRatio))
+        }
+
+        /*
+        if targetRatio < 1.0 {
+            if lastRatio < 1.0 {
+                newDiff = float64(prevDiff) * (1.0 - ((1.0 - lastRatio) * (1/targetRatio)))
+            }
+            if lastRatio > 1.0 {
+                newDiff = float64(prevDiff) * (1.0 + ((lastRatio - 1.0) * targetRatio))
+            }
+        }
+        if targetRatio > 1.0 {
+            if lastRatio < 1.0 {
+                newDiff = float64(prevDiff) * (1.0 - ((1.0 - lastRatio) * (1/targetRatio)))
+            }
+            if lastRatio > 1.0 {
+                newDiff = float64(prevDiff) * (1.0 + ((lastRatio - 1.0) * targetRatio))
+            }
+        }
+        */
+
+	return int64(newDiff)
+
+        /*
 	if targetRatio > 1.0 {
 		return int64(float64(prevDiff) * targetRatio)
 	}
 	if lastRatio < 1.0 {
 		return int64(float64(prevDiff) * targetRatio)
 	}
-	return int64(float64(prevDiff) * lastRatio)
+        */
+
+	//return int64(float64(prevDiff) * lastRatio)
+
 	//if newDiff < s.params.MinimumStakeDiff {
 	//	return s.params.MinimumStakeDiff
 	//}
